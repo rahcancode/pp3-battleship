5b052bcd75ad255144e1abe97c454a1714998c6c
Sarah A <sarahmoore26@gmail.com>
2023-08-13 13:09:52 +0000

    Add validation for placing ships and to update grid

--- run.py
+++ run.py
@@ -1,6 +1,5 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+import random
+import time
 
 """
     -------BATTLESHIP-------
@@ -15,7 +14,22 @@
     1. "~" = water or empty space
     2. "0" = part of ship
     3. "X" = part of ship that was hit with bullet
-    4. "#" = water that was shot with bullet, a miss because it hit no ship
+    4. "#" = a miss, only water was shot
 """
+GRID_SIZE = 10
+NUM_OF_SHIPS = 6
+MAX_BULLETS = 30
+ALPHABET = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+def validate_grid_and_place_ship(grid, ship_positions, start_row, end_row, start_col, end_col):
+    """Check if it's safe to place a ship on the grid and update the grid accordingly"""
+    all_valid = all(grid[r][c] == "." for r in range(start_row, end_row) for c in range(start_col, end_col))
+    if all_valid:
+        ship_positions.append((start_row, end_row, start_col, end_col))
+        for r in range(start_row, end_row):
+            for c in range(start_col, end_col):
+                grid[r][c] = "O"
+    return all_valid
+
 # Text image of a battleship
 f = open('battleship_art.txt', 'r')
